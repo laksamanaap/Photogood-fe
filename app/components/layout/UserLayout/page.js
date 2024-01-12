0da2ce521fe8c5fabd18947a087ff4af9b5cac97
laksamanaap <laksamana.arya1412@gmail.com>
2024-01-12 22:05:54 +0700

    setup reuseable component

--- app/components/layout/UserLayout/page.js
+++ app/components/layout/UserLayout/page.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Navbar } from "./Navbar";
-import { Footer } from "./Footer";
+import { Navbar } from "../Navbar";
+import { Footer } from "../Footer";
 
-export const AdminLayout = ({ children }) => {
+export const UserLayout = ({ children }) => {
   return (
     <>
       <Navbar />
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { UserLayout } from "./components/layout/UserLayout";
+import { UserLayout } from "./components/layout/UserLayout/page";
 
 const Home = () => {
   return (
