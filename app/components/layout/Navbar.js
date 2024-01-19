5ddf50136a47460491c84168ffa701fb70fdf518
laksamanaap <laksamana.arya1412@gmail.com>
2024-01-19 09:49:01 +0700

    feat : example photos assets

--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -1,5 +1,12 @@
 import React from "react";
+import sutek from "../../assets/images/favicon.png";
+import Image from "next/image";
 
 export const Navbar = () => {
-  return <div className="container">Navbar</div>;
+  return (
+    <div className="container">
+      Navbar
+      <Image src={sutek} alt="sutek" />
+    </div>
+  );
 };
