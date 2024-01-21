475570684b03cee6e50248063d2ff9e52507ba5d
laksamanaap <laksamana.arya1412@gmail.com>
2024-01-21 21:20:27 +0700

    feat : setup axios

--- app/utils/axiosHelpers.js
+++ app/utils/axiosHelpers.js
@@ -1,7 +1,7 @@
 import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_DOMAIN,
   params: {
     token: "$2y$12$I.VLboReme8.LROhx1ehx.N7y6Mu5vkSD6ySemUJKdhUtTqyJRGMe", // Temporary, soon will be updated using localStorage or cookies
   },
